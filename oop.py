b4bd05ce0ff2270a1d3a7e98681a196adac179f5
Edith Kosgey <kosgeyedith020@gmail.com>
2025-11-29 13:30:55 +0300

    update on my file

--- oop.py
+++ oop.py
@@ -1,43 +1,51 @@
-# #polymorphism
-# class animal:
-#     def __init__(self, name=None, age=None):
-#         self.name = name
-#         self.age = age
+#polymorphism
+class animal:
+    def __init__(self, name=None, age=None):
+        self.name = name
+        self.age = age
 
-# class dog(animal):
-#     def speak(self):
-#         return "bark"
+class dog(animal):
+    def speak(self):
+        return "bark"
 
-# class cat(animal):
-#     def speak(self):
-#         return "meow"
+class cat(animal):
+    def speak(self):
+        return "meow"
 
-# class lion(animal):
-#     def speak(self):
-#         return "roar"
+class lion(animal):
+    def speak(self):
+        return "roar"
 
-# animals = [dog("Rex", 3), cat("Whiskers", 2), lion("Simba", 5)]
-# for animal in animals:
-#     print(animal.speak())
-#     print(f"Name: {animal.name}, Age: {animal.age}")
-# #operator overloading
-# class matrix:
-#     def __init__(self, data):
-#         self.data = data
-#     def __add__(self, other):
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
+animals = [dog("Rex", 3), cat("Whiskers", 2), lion("Simba", 5)]
+for animal in animals:
+    print(animal.speak())
+    print(f"Name: {animal.name}, Age: {animal.age}")
+#operator overloading
+class matrix:
+    def __init__(self, data):
+        self.data = data
+    def __add__(self, other):
+        result = []
+        for i in range(len(self.data)):
+            row = []
+            for j in range(len(self.data[0])):
+                row.append(self.data[i][j] + other.data[i][j])
+            result.append(row)
+        return matrix(result)
 
-#     # Overload the - operator
-#     def __sub__(self, other):
-#         return Point(self.x - other.x, self.y - other.y)
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-# p1 = Point(5, 3)
-# p2 = Point(4, 1)
+    # Overload the - operator
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
 
-# result = p1 - p2
-# print(result.x, result.y)
+p1 = Point(5, 3)
+p2 = Point(4, 1)
+
+result = p1 - p2
+print(result.x, result.y)
 
 
